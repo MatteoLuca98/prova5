0a3369824bb4fa6a4248ee80a4ce0534c24107d2
matteo luca <matteo.luca@studenti.unitn.it>
2019-01-15 17:38:10 +0100

    modify square.js

--- square.js
+++ square.js
@@ -1,12 +1,16 @@
 function square(a,b,c,d){
-    if(a === b && c === d){
-        return 1;
-    }else if(a === c && b === d){
-        return 1;
-    }else if(a === d && b === c){
-        return 1;
+    if(!isNaN(a) && !isNaN(b) && !isNaN(c) && !isNaN(d)){
+        if(a === b && c === d){
+            return 1;
+        }else if(a === c && b === d){
+            return 1;
+        }else if(a === d && b === c){
+            return 1;
+        }else{
+            return -1;
+        }  
     }else{
-        return -1;
-    }  
+        return -2;
+    }   
 }
 module.exports = square;
