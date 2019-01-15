4bc2f6d649a513d4d238c06d7bfe272bfc703442
matteo luca <matteo.luca@studenti.unitn.it>
2019-01-15 18:11:06 +0100

    modify app.test.js

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
 app.get('/square', function (req,res){
-    res.status(200).send({result : square(req.query.number)});
+    res.status(200).send({result : square(req.query.number,number,number,number)});
 });
 module.exports = app;
--- app.test.js
+++ app.test.js
@@ -1,8 +1,8 @@
 const request = require('supertest');
 const app = require('./app');
 
-test('GET/number= number should return 200', async () =>{
-    const response = await request(app).get('/square ?number= 1,4,1,4');
+test('GET/number= number, number, number, number should return 200', async () =>{
+    const response = await request(app).get('/square?number=' + (1,4,1,4));
     expect(response.statusCode).toBe(200);
     expect(response.body.result).toBe(1);
 });
