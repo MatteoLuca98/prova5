7c5f4f4c74da67243de12939b2fef69b45a2adfb
matteo luca <matteo.luca@studenti.unitn.it>
2019-01-15 17:45:18 +0100

    modify app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const square = require('./square');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
+app.get('/square', function (req,res){
+    res.status(200).send({result : square(req.query.number)});
+});
 module.exports = app;
